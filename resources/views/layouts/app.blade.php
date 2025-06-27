231ad2d39c9b60c28d0775ff4991670641127b6c
aditiaNovember01 <adtianovirman01@gmail.com>
2025-06-27 19:46:57 +0700

    layout backend

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Dashboard')</title>
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+    @stack('styles')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        @include('layouts.partials.navbar')
+        @include('layouts.partials.sidebar')
+        <div class="content-wrapper">
+            <section class="content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </section>
+        </div>
+        @include('layouts.partials.footer')
+    </div>
+    <!-- AdminLTE JS -->
+    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/adminlte.min.js') }}"></script>
+    @stack('scripts')
+</body>
+
+</html>
